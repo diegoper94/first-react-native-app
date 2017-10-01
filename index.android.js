b97f507a108ee8b6aa3589cb5897349fe9a7e174
diegoper94 <diegoperdomo94@gmail.com>
2017-10-01 14:14:30 -0400

    add styles to header component and making it reusable

--- index.android.js
+++ index.android.js
@@ -4,7 +4,7 @@ import Header from './src/components/header';
 
 const App = () => (
   <View>
-    <Header />
+    <Header headerText={'Albums'} />
   </View>
 );
 
--- src/components/header.js
+++ src/components/header.js
@@ -1,6 +1,26 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, View } from 'react-native';
 
-const Header = () => <Text>Albums!</Text>;
+const Header = props => (
+  <View style={styles.viewStyle}>
+    <Text style={styles.textStyle}>{props.headerText}</Text>
+  </View>
+);
+
+const styles = {
+  viewStyle: {
+    backgroundColor: '#d0ddf2',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 60,
+    elevation: 10,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.2
+  },
+  textStyle: {
+    fontSize: 20
+  }
+};
 
 export default Header;
